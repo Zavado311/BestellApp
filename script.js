d9f2b55e3d0053d4ecf3583f3ab48c73beba7b30
Zavado311 <brian.wigger@bluewin.ch>
2024-09-19 10:41:37 -0500

    before responsive menu

--- script.js
+++ script.js
@@ -77,3 +77,11 @@ function sumBasket() {
   sumUp = [];
   return total.toFixed(2);
 }
+
+function showMenu() {
+  document.getElementById('menu').classList.add('show-overlay-menu');
+}
+
+function closeMenu() {
+  document.getElementById('menu').classList.remove('show-overlay-menu');
+}
